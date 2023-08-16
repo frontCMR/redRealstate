a50d7a0e6a9a1580db00275bc041e7fe97201677
JUAN-BH <juanmita_bh@hotmail.com>
2023-08-16 07:44:29 -0500

    arreglando footer

--- src/global/Footer/FooterApp.vue
+++ src/global/Footer/FooterApp.vue
@@ -1,12 +1,34 @@
 <template>
-  <footer class=" bg-footerBg">
+  <footer class="bg-footerBg">
     <section
       class="flex flex-col gap-6 max-w-screen-xl mx-auto p-6 text-white md:flex-row md:items-center md:justify-between"
     >
       <article class="flex flex-col gap-4">
         <div>
-          <h3 class="footerTitle">Cali</h3>
-          <p>Calle 5 No. 85 - 29 - Barrio las Vegas (Cali)</p>
+          <h3 class="footerTitle">Oficinas Colombia</h3>
+          <div>
+            <h3>Cali</h3>
+            <p>Calle 5 No. 85 – 29 Barrio Las vegas</p>
+          </div>
+          <div>
+            <h3>Bogota</h3>
+            <p>Edificio Emprendu Carrera 13 No. 83 – 19 piso 2 Bogotá</p>
+          </div>
+          <div>
+            <h3>Medellin</h3>
+            <p>Cl. 18 #35-69, Mall Palms Avenue Oficina 9749 S-3,</p>
+          </div>
+          <div>
+            <h3>Cajicá</h3>
+            <p>
+              Sabana Park Health & Business Carrera 5 # 9 – 26 Sur, Cajicá,
+              Cundinamarca
+            </p>
+          </div>
+          <div>
+            <h3>Barranquilla</h3>
+            <p>Edificio Torre Atlántica Carrera 53 No. 80 – 198 Piso 17</p>
+          </div>
         </div>
         <div>
           <h3 class="footerTitle">Contacto</h3>
@@ -15,8 +37,13 @@
         </div>
         <p><span class="font-bold">Email:</span> contacto@red-realestate.com</p>
         <div class="flex items-center gap-4">
-          <a v-for="item in socialMedia" :key="item.link" :href="item.link" target="_blank">
-            <img :src="item.icon" alt="">
+          <a
+            v-for="item in socialMedia"
+            :key="item.link"
+            :href="item.link"
+            target="_blank"
+          >
+            <img :src="item.icon" alt="" />
           </a>
           <!-- <img src="./assets/icons/iconFacebook.png" alt=""> -->
         </div>
@@ -32,34 +59,33 @@
   </footer>
 </template>
 <script setup lang="ts">
-  import {ref, Ref} from 'vue'
-  interface SocialMedia {
-    link: string
-    icon: string
-  }
-  const socialMedia: Ref<SocialMedia[]> = ref([
-    {
-      link: 'https://www.instagram.com/red_realestatedigital/',
-      icon: 'https://res.cloudinary.com/dumnq4c3n/image/upload/v1692106316/iconInstagram_t9tm78.png',
-    },
-    {
-      link: 'https://www.facebook.com/red.realestate2023/',
-      icon: 'https://res.cloudinary.com/dumnq4c3n/image/upload/v1692106316/iconFacebook_yfw5ig.png',
-    },
-    {
-      link: 'linkedIn',
-      icon: 'https://res.cloudinary.com/dumnq4c3n/image/upload/v1692106316/iconLinkedIn_byu1tq.png',
-    },
-    {
-      link: `https://wa.me/+573244047878`,
-      icon: 'https://res.cloudinary.com/dumnq4c3n/image/upload/v1692106316/iconWhatsapp_wgmbqo.png',
-    },
-    {
-      link: 'https://www.youtube.com/channel/UC0hJEfhcWzbtSJeeJjYHmMw',
-      icon: 'https://res.cloudinary.com/dumnq4c3n/image/upload/v1692106316/iconYoutube_iuth71.png',
-    },
-  ])
-
+import { ref, Ref } from "vue";
+interface SocialMedia {
+  link: string;
+  icon: string;
+}
+const socialMedia: Ref<SocialMedia[]> = ref([
+  {
+    link: "https://www.instagram.com/red_realestatedigital/",
+    icon: "https://res.cloudinary.com/dumnq4c3n/image/upload/v1692106316/iconInstagram_t9tm78.png",
+  },
+  {
+    link: "https://www.facebook.com/red.realestate2023/",
+    icon: "https://res.cloudinary.com/dumnq4c3n/image/upload/v1692106316/iconFacebook_yfw5ig.png",
+  },
+  {
+    link: "linkedIn",
+    icon: "https://res.cloudinary.com/dumnq4c3n/image/upload/v1692106316/iconLinkedIn_byu1tq.png",
+  },
+  {
+    link: `https://wa.me/+573244047878`,
+    icon: "https://res.cloudinary.com/dumnq4c3n/image/upload/v1692106316/iconWhatsapp_wgmbqo.png",
+  },
+  {
+    link: "https://www.youtube.com/channel/UC0hJEfhcWzbtSJeeJjYHmMw",
+    icon: "https://res.cloudinary.com/dumnq4c3n/image/upload/v1692106316/iconYoutube_iuth71.png",
+  },
+]);
 </script>
 <style lang="css">
 .footerTitle {
